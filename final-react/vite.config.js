84cb946853842fe3aa9ca6905e4b541cab26e6fc
Wilenys <andrea_9404@hotmail.com>
2025-12-05 17:46:16 -0300

    arreglo index

--- final-react/vite.config.js
+++ final-react/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Proyecto-Final-React/', // 🔹 IMPORTANTE: el nombre de tu repo
+});
+
